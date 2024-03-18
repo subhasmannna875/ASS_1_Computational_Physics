b10c9e77ca629b6cd3ffb8d4525167089b871407
subhas manna <subhasmanna875@gmail.com>
2024-03-18 22:44:00 +0530

    Q16_Jacobi method to solve system of linear eqation

---
+++ Q16_jacobi_methode.py
@@ -0,0 +1,31 @@
+import numpy as np
+#actual_solution
+x_act=np.array([7.859713071,0.422926408,-0.073592239,-0.540643016,0.010626163])
+infinity_norm=np.max(np.abs(x_act))
+
+# Define the coefficient matrix A and the constant vector b
+A = np.array([[0.2,0.1,1,1,0],
+              [0.1,4,-1,1,-1],
+              [1,-1,60,0,-2],
+              [1,1,0,8,4],
+              [0,-1,-2,4,700]])
+b = np.array([1,2,3,4,5])
+
+# Decompose A into diagonal (D), lower triangular (L), and upper triangular (U) parts
+D = np.diag(np.diag(A))
+L = np.tril(A, k=-1)
+U = np.triu(A, k=1)
+
+# Initial guess for x
+x = np.zeros_like(b)
+
+# Jacobi iteration
+max_iterations = 1000
+
+for i in range(max_iterations):
+    x_new = np.linalg.inv(D).dot(b - (L + U).dot(x))
+    if np.max(np.abs(x_new - x_act)) / np.max(np.abs(x_act)) < 0.01:
+        break
+    x = x_new
+print("Solution:", x)
+print("number of iteration:",i+1)
