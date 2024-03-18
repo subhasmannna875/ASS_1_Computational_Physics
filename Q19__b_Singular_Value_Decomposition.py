cf17cf79800ef0c70402c46126a51c54cdc12526
subhas manna <subhasmanna875@gmail.com>
2024-03-18 23:10:26 +0530

    Q19_2nd_Matrix_singular_value_decomposition

---
+++ Q19__b_Singular_Value_Decomposition.py
@@ -0,0 +1,37 @@
+import numpy as np
+import time
+
+# Define the matrix
+A = np.array([[2, 1],
+              [1, 0],
+              [0, 1]])
+
+# Time the computation of SVD
+start_time = time.time()
+U, S, VT = np.linalg.svd(A)
+end_time = time.time()
+
+# Determine the number of singular values
+num_singular_values = min(A.shape)# minimum value of 'm'  or 'n'
+
+
+# Construct the diagonal matrix from  singular values
+Sigma = np.zeros_like(A, dtype=float)
+Sigma[:num_singular_values, :num_singular_values] = np.diag(S)
+
+# Compute the reconstructed matrix using rounded singular values
+reconstructed_A = U @ Sigma @ VT
+reconstructed_A=np.round(reconstructed_A,decimals=8)
+# Print the results
+print("Matrix A:")
+print(A)
+print("\nSingular Value Decomposition (SVD) with rounded singular values:")
+print("U:")
+print(U)
+print("\nSingular Values (S) rounded to four decimal places:")
+print(Sigma)
+print("\nV^T:")
+print(VT)
+print("\nReconstructed Matrix with rounded singular values:")
+print(reconstructed_A)
+print("\nTime taken for computation: {} seconds".format(end_time - start_time))
