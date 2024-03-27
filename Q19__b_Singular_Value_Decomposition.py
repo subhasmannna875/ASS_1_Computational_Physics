4a1b7f1193137170e0eff410d0cca1486e620298
subhas manna <subhasmanna875@gmail.com>
2024-03-27 16:51:54 +0530

    Q19 _singuler value decomposition of second matrix

--- Q19__b_Singular_Value_Decomposition.py
+++ Q19__b_Singular_Value_Decomposition.py
@@ -35,3 +35,36 @@ print(VT)
 print("\nReconstructed Matrix with rounded singular values:")
 print(reconstructed_A)
 print("\nTime taken for computation: {} seconds".format(end_time - start_time))
+
+
+'''
+out put :
+
+
+Matrix A:
+[[2 1]
+ [1 0]
+ [0 1]]
+
+Singular Value Decomposition (SVD) with rounded singular values:
+U:
+[[-9.12870929e-01  5.55111512e-17 -4.08248290e-01]
+ [-3.65148372e-01 -4.47213595e-01  8.16496581e-01]
+ [-1.82574186e-01  8.94427191e-01  4.08248290e-01]]
+
+Singular Values (S) rounded to four decimal places:
+[[2.44948974 0.        ]
+ [0.         1.        ]
+ [0.         0.        ]]
+
+V^T:
+[[-0.89442719 -0.4472136 ]
+ [-0.4472136   0.89442719]]
+
+Reconstructed Matrix with rounded singular values:
+[[ 2.  1.]
+ [ 1. -0.]
+ [-0.  1.]]
+
+Time taken for computation: 0.0023326873779296875 seconds
+'''
